74f681d3d388e613e529231d261f7c2e8440bc7c
Valdo <83594809+ValdoGudino@users.noreply.github.com>
2023-08-08 01:06:50 -0500

    Match Figure 2.1-9

--- measles_number.py
+++ measles_number.py
@@ -12,24 +12,20 @@ def main():
     print('----------------------------\n\n')
 
     population: float = 0.0
-    population_target: float = 1.0
-    popluation_step: float = 0.01
     p: float = 0.3
 
-    feedback: float = 0.0
-    feedback_target: float = 3.0
-    feedback_step: float = 0.05
+    feedback_target: float = 3.5
+    feedback_step: float = 0.5
 
-    maximal_iteration: int = 30
+    maximal_iteration: int = 10
 
     d: list = []
 
-    for p in np.arange(0, population_target, popluation_step):
+    for k in np.arange(0.5, feedback_target, feedback_step):
         population = p
-        for k in np.arange(0.5, feedback_target, feedback_step):
-            for i in range(maximal_iteration):
-                population = f(population, k)
-                d.append([k, p, population])
+        for i in range(maximal_iteration):
+            population = f(population, k)
+            d.append([k, p, population])
 
     df = pd.DataFrame(
         data=d, columns=['feedback', 'initial_population', 'terminal_population'])
