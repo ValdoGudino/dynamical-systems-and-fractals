c382b110700ea1e74fb19e764892ea87757b10b2
Valdo <83594809+ValdoGudino@users.noreply.github.com>
2023-08-08 21:58:15 -0500

    Add in examples working up to bifurcation

--- README.md
+++ README.md
@@ -2,4 +2,14 @@
 
 ## Calculation of Measles-Values
 
-![plot](measles_number.png)
+### Small K Value Set
+
+![plot](./images/small_k_value_set.png)
+
+### Larger K Value Set
+
+![plot](./images/larger_k_value_set.png)
+
+### Bifurcation
+
+![plot](./images/bifurcation.png)
--- measles_number.py
+++ measles_number.py
@@ -7,14 +7,63 @@ def f(p: float, k: float):
     return p + k * p * (1 - p)
 
 
-def main():
-    print('Calculation of Measles-Values')
-    print('----------------------------\n\n')
-
+def small_k_value_set():
     population: float = 0.0
     p: float = 0.3
 
-    feedback_target: float = 3.5
+    feedback_target: float = 3.0
+    feedback_step: float = 0.5
+
+    maximal_iteration: int = 10
+
+    d: list = []
+
+    for k in np.arange(0.5, feedback_target + feedback_step, feedback_step):
+        population = p
+        for i in range(maximal_iteration):
+            population = f(population, k)
+            d.append([k, population])
+
+    df = pd.DataFrame(
+        data=d, columns=['feedback', 'terminal_population'])
+    fig = px.scatter(df, x='feedback',  y='terminal_population')
+    fig.update_traces(marker=dict(size=5, line=dict(width=1, color="Black")),
+                      selector=dict(mode='markers'))
+    fig.update_layout(xaxis_range=[0.4, 3.1], yaxis_range=[0, 1.5])
+    fig.show()
+
+
+def larger_k_value_set():
+    population: float = 0.0
+    p: float = 0.3
+
+    feedback_target: float = 3.0
+    feedback_step: float = 0.001
+
+    maximal_iteration: int = 50
+
+    d: list = []
+
+    for k in np.arange(0.5, feedback_target + feedback_step, feedback_step):
+        population = p
+        for i in range(maximal_iteration):
+            population = f(population, k)
+            d.append([k, population])
+
+    df = pd.DataFrame(
+        data=d, columns=['feedback', 'terminal_population'])
+    fig = px.scatter(df, x='feedback',  y='terminal_population')
+    fig.update_traces(marker=dict(size=1, line=dict(width=1, color="Black")),
+                      selector=dict(mode='markers'))
+    fig.update_layout(xaxis_range=[0.5, 3.0], yaxis_range=[0, 1.5])
+    fig.show()
+
+
+def bifurcation():
+    population: float = 0.0
+    p: float = 0.3
+
+    feedback_target: float = 3.0
     feedback_step: float = 0.0001
 
     maximal_iteration: int = 300
@@ -38,6 +87,14 @@ def main():
                       selector=dict(mode='markers'))
     fig.update_layout(xaxis_range=[1.90, 3], yaxis_range=[0, 1.5])
     fig.show()
+
+
+def main():
+    print('Calculation of Measles-Values')
+    print('----------------------------\n\n')
+    small_k_value_set()
+    larger_k_value_set()
+    bifurcation()
 
 
 if __name__ == '__main__':
